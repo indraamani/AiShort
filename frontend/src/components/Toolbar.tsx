a0911b2373e61e61bf37ca90c36e3b22e473bf3e
indraamani <indraamani24@gmail.com>
2026-05-24 22:26:53 +0530

    chore: add dependency

---
+++ frontend/src/components/Toolbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Toolbar() {
+  return (
+    <nav className='flex bg-black h-16 absolute top-4 w-full mr-4  text-white'>
+
+        <div className="icon">
+            <h3>Pixel.io</h3>
+        </div>
+
+        <section className="">
+            <div className="icon">
+
+            </div>
+
+            <menu type="toolbar">
+
+            </menu>
+            <menu type="toolbar">
+
+            </menu>
+        </section>
+        
+    </nav>
+  )
+}
+
+export default Toolbar
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Toolbar from "../components/Toolbar"
+
+function Home() {
+  return (
+    <div>
+      <Toolbar />
+    </div>
+  )
+}
+
+export default Home
